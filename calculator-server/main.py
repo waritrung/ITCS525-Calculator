57dfb5280fa3ef1a743047972e70d95dd23540e8
Warit <waritrung@gmail.com>
2025-08-19 13:56:20 +0700

    feat(calculator): - deploy calculator program - deploy test case

---
+++ calculator-server/main.py
@@ -0,0 +1,63 @@
+import math
+from collections import deque
+from datetime import datetime
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from asteval import Interpreter
+
+from calculator import expand_percent
+
+HISTORY_MAX = 1000
+history = deque(maxlen=HISTORY_MAX)
+
+app = FastAPI(title="Mini Calculator API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ---------- Safe evaluator ----------
+aeval = Interpreter(minimal=True, usersyms={"pi": math.pi, "e": math.e})
+
+
+@app.post("/calculate")
+def calculate(expr: str):
+    try:
+        now = datetime.now()
+        
+        expr = expr.replace("×", "*")
+        expr = expr.replace("÷", "/")
+
+        code = expand_percent(expr)
+        result = aeval(code)
+        if aeval.error:
+            msg = "; ".join(str(e.get_error()) for e in aeval.error)
+            aeval.error.clear()
+            return {"ok": False, "expr": expr, "result": "", "error": msg}
+        
+        # TODO: Add history 
+        historyInfo = {
+            "timestamp": now, 
+            "expr": expr, 
+            "result": result}
+        
+        history.append(historyInfo)
+
+        return {"ok": True, "expr": expr, "result": result, "error": ""}
+    except Exception as e:
+        return {"ok": False, "expr": expr, "error": str(e)}
+
+# TODO GET /hisory
+@app.get("/history")
+def get_History(limit: int):
+
+        return history
+
+# TODO DELETE /history
+@app.delete("/history")
+def clear_History():
+    history.clear()
+    return {"ok": True, "cleared": True}
